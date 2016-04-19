33cd1b8279285e0edb9be15ae3ed8f0a4dbde12e
kkloberdanz <kyle.g.kloberdanz@uiowa.edu>
2016-04-18 20:53:52 -0500

    api improved

--- ledpiapi.py
+++ ledpiapi.py
@@ -1,11 +1,14 @@
 import requests
-def turn_off(light):
-    url = "http://192.168.0.5:8000/piserver/" + str(light) + "/flip"
-    selection = ", {'choice':['1']}"
-    requests.post(url+selection)
 
-def turn_on(light):
-    url = "http://192.168.0.5:8000/piserver/" + str(light) + "/flip"
-    selection = ", {'choice':['2']}"
-    requests.post(url+selection)
+def flip_light(light, position, webpage, intensity=100):
+    # url = 'http://192.168.0.7:8000/piserver/' + str(light) + '/flip/'
+    url = webpage + str(light) + '/flip/'
+    if position == "off":
+        choice = str(2)
+    elif position == "on":
+        choice = str(1)
+    else:
+        print("flip_light: only options are \'on\' and \'off\'")
+    # command = str(", {\'choice\':[\'" + choice + "\']}")
+    requests.post(url, {'choice':[choice]})
 
